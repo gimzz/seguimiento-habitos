aa6b6c5335df2ed4f555485e9d0352d978214708
gimzz <letig732@gmail.com>
2025-09-23 14:34:43 -0400

    test

---
+++ src/test/java/com/mycompany/seguimiento_habitos/SeguimientoHabitosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.seguimiento_habitos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeguimientoHabitosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
